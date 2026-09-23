e0c512b07f4fc6dae4340770cbf95dff73d7f10c
Vitaly Kalinin <vtly.kl@gmail.com>
2026-09-24 00:13:57 +0300

    Use RandomNumberGenerator.GetInt32 to remove modulo bias in GenerateRandomPassword

--- ProjectBeacon.Infrastructure/Security/PasswordHasher.cs
+++ ProjectBeacon.Infrastructure/Security/PasswordHasher.cs
@@ -12,7 +12,6 @@ public static class PasswordHasher
     public static string GenerateRandomPassword(int length = 32)
     {
         const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
-        var bytes = RandomNumberGenerator.GetBytes(length);
-        return new string(Enumerable.Range(0, length).Select(_ => chars[bytes[_] % chars.Length]).ToArray());
+        return new string(Enumerable.Range(0, length).Select(_ => chars[RandomNumberGenerator.GetInt32(0, chars.Length)]).ToArray());
     }
 }
